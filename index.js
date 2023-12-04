f9d1489b030a200bd21eb979982d2c1327c2904a
sagor009988 <soheltashjid001@gmail.com>
2023-12-04 12:33:50 +0600

    all oke

--- index.js
+++ index.js
@@ -313,8 +313,8 @@ app.put("/api/submitResult/:id", (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
